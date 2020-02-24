b99844beb5d8ebcb386fd71da439e095bec59f42
engg-autologin <engg-autologin@users.campus>
2020-02-24 17:12:53 -0600

    In-class work

---
+++ ShoppingList3/AddItemControl.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ShoppingList3
+{
+    /// <summary>
+    /// Interaction logic for AddItemControl.xaml
+    /// </summary>
+    public partial class AddItemControl : UserControl
+    {
+        public AddItemControl()
+        {
+            InitializeComponent();
+            AddItemButton.Click += OnAddItemButtonClick;
+        }
+
+        protected void OnAddItemButtonClick(object sender, RoutedEventArgs args)
+        {
+            if(DataContext is ShoppingListData data)
+            {
+                data.Add(ItemToAdd.Text);
+                ItemToAdd.Text = "";
+            }
+        }
+    }
+}
---
+++ ShoppingList3/ShoppingListDisplay.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ShoppingList3
+{
+    /// <summary>
+    /// Interaction logic for ShoppingListDisplay.xaml
+    /// </summary>
+    public partial class ShoppingListDisplay : UserControl
+    {
+        public ShoppingListDisplay()
+        {
+            InitializeComponent();
+        }
+
+        public void OnDeleteItemButtonClicked(object sender, RoutedEventArgs e)
+        {
+            if(DataContext is ShoppingListData data)
+            {
+                if (sender is Button button)
+                {
+                    data.Remove(button.DataContext.ToString());
+                }
+            }
+        }
+    }
+}
